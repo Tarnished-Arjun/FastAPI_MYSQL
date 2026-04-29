da6b487cf65f401a1838aac71f29815a84f9a0d0
Arjun <arjunkrishna0081@gmail.com>
2026-04-29 13:00:14 +0530

    Delete and update added

--- Relations/crud.py
+++ Relations/crud.py
@@ -5,21 +5,48 @@ from .models import User, UserProfile
 def create_user_with_profile(db: Session, name: str, bio: str):
     user = User(name=name)
 
+    profile = UserProfile(bio=bio)
+
+    user.profile = profile
+
     db.add(user)
     db.commit()
+
     db.refresh(user)
-
-    profile = UserProfile(
-        bio=bio,
-        user_id=user.id
-    )
-
-    db.add(profile)
-    db.commit()
     db.refresh(profile)
 
     return user, profile
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+    user = db.query(User).filter(User.id == user_id).first()
+    print(user.profile)
+    return user
+
+
+def update_user_with_profile(db: Session, user_id: int, name: str, bio: str):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user:
+        user.name = name
+
+        if user.profile:
+            user.profile.bio = bio
+
+        db.commit()
+        db.refresh(user)
+
+    return user
+
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user:
+        if user.profile:
+            db.delete(user.profile)
+
+        db.delete(user)
+        db.commit()
+
+    return user
